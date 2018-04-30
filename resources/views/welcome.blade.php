b85e60429bf9b13bb9806498a3e4d328c6c3d8c4
hosang <hosang@chain.partners>
2018-04-30 20:24:39 +0900

    사용자 인증

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.master')
 
+@if(false)
+
 @section('content')
     <p>저는 자식 뷰의 'content' 섹션입니다.</p>
 
@@ -25,3 +27,5 @@
         alert("저는 자식 뷰의 'script' 섹션입니다.");
     </script>
 @endsection
+
+@endif
