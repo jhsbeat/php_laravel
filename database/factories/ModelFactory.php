7a0c3b4a4f356aa7d4307f5676ea7bd4b56ecc42
hosang <hosang@chain.partners>
2018-05-02 22:44:58 +0900

    데이터베이스 시딩

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -17,7 +17,19 @@ $factory->define(App\User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        'password' => bcrypt('password'),
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Article::class, function (Faker $faker) {
+    $date = $faker->dateTimeThisMonth;
+
+    return [
+        'title' => $faker->sentence,
+        'content' => $faker->paragraph,
+        'created_at' => $date,
+        'updated_at' => $date
+    ];
+});
+
