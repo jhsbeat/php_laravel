6758fd6e35ce7c509c6285db13b680768d6a733f
hosang <hosang@chain.partners>
2018-05-07 11:52:04 +0900

    컨트롤러와 도우미 함수

--- routes/web.php
+++ routes/web.php
@@ -76,8 +76,4 @@ EOT;
     return app(ParsedownExtra::class)->text($text);
 });
 
-Route::get('docs/{file?}', function($file = null){
-    $text = (new \App\Documentation)->get($file);
-
-    return app(ParsedownExtra::class)->text($text);
-});
+Route::get('docs/{file?}', 'DocsController@show');
